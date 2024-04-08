17834521370c6577e6ac38a65f87ca1088ac6049
Aniket Pratap Singh <114793936+Aniket-1100@users.noreply.github.com>
2024-04-08 09:36:28 +0530

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,11 @@
+public class Stack{
+    public static void main(String[] args){
+            ABC obj=new ABC();
+            obj.ABC();
+    }
+}
+class ABC{
+    void ABC(){
+        ABC();
+    }
+}
