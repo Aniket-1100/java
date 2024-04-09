0558641fed052b0c6015dbd3137a849c972ff684
Aniket Pratap Singh <114793936+Aniket-1100@users.noreply.github.com>
2024-04-09 11:26:47 +0530

    Add files via upload

---
+++ TypeInterface2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class TypeInterface2{
+public static void main(String[] args){
+List<String> list= new ArrayList<String>();
+list.add("Hello");
+for(String element:list){
+System.out.println(element);
+}
+List<String> list2= new ArrayList<String>();
+list2.add("I'm");
+for(String element:list2){
+System.out.println(element);
+}
+List<String> list3= new ArrayList<String>();
+list3.add("fine");
+for(String element:list3){
+System.out.println(element);
+}
+List<String> list4=new ArrayList<String>();
+list4.add("roll no.: 31");
+for(String element:list4){
+System.out.println(element);
+}
+}
+}
